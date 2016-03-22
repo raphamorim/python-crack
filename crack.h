4416c33f3dd0b8f0526995ddd952c1bbdb81a79b
Raphael Amorim <rapha850@gmail.com>
2016-03-22 15:11:00 -0300

    Update crack.h

--- crack.h
+++ crack.h
@@ -1,76 +1,33 @@
-/*
-    2  *
-    3  *                 #####    #####   ######  ######  ###   ###
-    4  *               ##   ##  ##   ##  ##      ##      ## ### ##
-    5  *              ##   ##  ##   ##  ####    ####    ##  #  ##
-    6  *             ##   ##  ##   ##  ##      ##      ##     ##
-    7  *            ##   ##  ##   ##  ##      ##      ##     ##
-    8  *            #####    #####   ##      ######  ##     ##
-    9  *
+#ifndef CRACKLIB_H
+#define CRACKLIB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Pass these functions a password (pw) and a path to the
+ * dictionaries (/usr/lib/cracklib_dict should be specified)
+ * and it will either return a NULL string, meaning that the
+ * password is good, or a pointer to a string that explains the
+ * problem with the password.
  *
- *             OOFEM : Object Oriented Finite Element Code
+ * FascistCheckUser() executes tests against an arbitrary user (the 'gecos'
+ * attribute can be NULL), while FascistCheck() assumes the currently logged
+ * in user.
  *
- *               Copyright (C) 1993 - 2013   Borek Patzak
- *
- *
- *
- *       Czech Technical University, Faculty of Civil Engineering,
- *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * You must link with -lcrack
  */
-#ifndef CRACK_H_
-#define CRACK_H_
- 
-#include "xfem/enrichmentitem.h"
-#include "xfem/hybridei.h"
- 
-#define _IFT_Crack_Name "crack"
 
-namespace oofem {
-class XfemManager;
-class Domain;
-class InputRecord;
-class GaussPoint;
-class GnuplotExportModule;
+extern const char *FascistCheck(const char *pw, const char *dictpath);
+extern const char *FascistCheckUser(const char *pw, const char *dictpath,
+				    const char *user, const char *gecos);
 
-class OOFEM_EXPORT Crack : public HybridEI
-{
-public:
-    Crack(int n, XfemManager *xm, Domain *aDomain);
+/* This function returns the compiled in value for DEFAULT_CRACKLIB_DICT.
+ */
+extern const char *GetDefaultCracklibDict(void);
 
-    virtual const char *giveClassName() const { return "Crack"; }
-    virtual const char *giveInputRecordName() const { return _IFT_Crack_Name; }
-    virtual IRResultType initializeFrom(InputRecord *ir);
-
-    void AppendCohesiveZoneGaussPoint(GaussPoint *ipGP);
-
-    virtual void callGnuplotExportModule(GnuplotExportModule &iExpMod, TimeStep *tStep);
-
-    const std :: vector< GaussPoint * > &giveCohesiveZoneGaussPoints() const { return mCohesiveZoneGaussPoints; }
-    const std :: vector< double > &giveCohesiveZoneArcPositions() const { return mCohesiveZoneArcPositions; }
-
-    void computeCrackIntersectionPoints(Crack &iCrack, std :: vector< FloatArray > &oIntersectionPoints, std :: vector< double > &oArcPositions);
-    void computeArcPoints(const std :: vector< FloatArray > &iIntersectionPoints, std :: vector< double > &oArcPositions);
-    double computeLength();
-    virtual int giveDofPoolSize() const;
-
-protected:
-    std :: vector< GaussPoint * >mCohesiveZoneGaussPoints;
-    std :: vector< double >mCohesiveZoneArcPositions;
+#ifdef __cplusplus
 };
-} // end namespace oofem
+#endif
 
-#endif /* CRACK_H_ */
+#endif
